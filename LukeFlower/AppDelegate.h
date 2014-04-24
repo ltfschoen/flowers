46b97165b4c9c9e41cd648bef5bcc1ace15e60a1
ltfschoen <ltfschoen@gmail.com>
2014-04-24 16:11:10 +1000

    Initial Commit

---
+++ LukeFlower/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LukeFlower
+//
+//  Created by Luke on 24/04/2014.
+//  Copyright (c) 2014 Luke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LukeFlowerTests/LukeFlowerTests.m
@@ -0,0 +1,34 @@
+//
+//  LukeFlowerTests.m
+//  LukeFlowerTests
+//
+//  Created by Luke on 24/04/2014.
+//  Copyright (c) 2014 Luke. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LukeFlowerTests : XCTestCase
+
+@end
+
+@implementation LukeFlowerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
